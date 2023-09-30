4d2a02a980ce7c69cea79e982f65c7c070b8dde5
Patrick <patrick.hergan@proton.me>
2023-09-30 13:34:23 +0200

    remove data from repo

--- migrations/2023-09-22-094041_create_talents/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-CREATE TABLE talents (
-    id Text PRIMARY KEY NOT NULL,
-    title Text NOT NULL,
-    maximum Text NOT NULL,
-    tests TEXT NOT NULL,
-    talent_desc Text NOT NULL
-);
